897e421a8f95a6c3623a61b674be2ba5704fd99a
Kuba Suder <jakub.suder@gmail.com>
2023-06-19 23:25:43 +0200

    added more timeline ids

--- lib/bad_pigeon/timelines.rb
+++ lib/bad_pigeon/timelines.rb
@@ -3,8 +3,20 @@ Dir[File.join(__dir__, 'timelines', '*.rb')].each { |f| require(f) }
 module BadPigeon
   TIMELINE_TYPES = {
     'UserTweets' => UserTimeline,
+    'UserMedia' => UserTimeline,
     'HomeLatestTimeline' => HomeTimeline,
     'HomeTimeline' => HomeTimeline,
-    'ListLatestTweetsTimeline' => ListTimeline
+    'ListLatestTweetsTimeline' => ListTimeline,
+    'CommunitiesTabBarItemQuery' => nil,
+    'DataSaverMode' => nil,
+    'GetUserClaims' => nil,
+    'ListByRestId' => nil,
+    'ListMembers' => nil,
+    'ListPins' => nil,
+    'ListSubscribers' => nil,
+    'ListsManagementPageTimeline' => nil,
+    'ProfileSpotlightsQuery' => nil,
+    'UserByScreenName' => nil,
+    'getAltTextPromptPreference' => nil,
   }
 end
